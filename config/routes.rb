350f8565b48d881047a7d8294da522ffa2ef2bfb
Ryan Hageman <improv42@gmail.com>
2017-12-18 20:25:06 -0600

    Add navbar and profiles views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles' => 'profiles#index'
+
+  get 'profiles/:id' => 'profiles#show', as: :profile
+
   resources :tweets
   devise_for :users
+
+  root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
